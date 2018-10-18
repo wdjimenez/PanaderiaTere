3cc0e3f926f499fb8dc072ddbf372673abc98996
Darío Jiménez <Daro Jimnez@DESKTOP-4OA6OUD>
2018-10-18 13:05:32 -0500

    Iniciar proyecto Ventana Productos Ventana Ventas

---
+++ src/model/ProductoModel.java
@@ -0,0 +1,43 @@
+package model;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Darío Jiménez
+ */
+public class ProductoModel {
+ private String nombre;
+ private float precio;
+ private int stock;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+ 
+ 
+}
