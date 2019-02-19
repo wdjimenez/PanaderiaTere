5d885aac50a9df7bbd0dc59cac41b87fd46cf79b
Darío Jiménez <Daro Jimnez@DESKTOP-4OA6OUD>
2019-02-19 10:34:27 -0600

    Reporte de Ventas, los campos importe y precio se muestran con decimales

--- src/pos/view/RepStock.java
+++ src/pos/view/RepStock.java
@@ -35,7 +35,7 @@ public class RepStock extends javax.swing.JFrame {
         tableStock = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Reporte de Existencia");
         setResizable(false);
 
